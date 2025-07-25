affbadb72037b2f0b711f41240b206d65a8c4ee4
Pranesh1003 <madhupranesh10@gmail.com>
2025-07-25 22:27:08 +0530

    week 6

--- Week 3/Spring Data JPA with Hibernate/9.Employee Management System - Customizing Data Source Configuration/Code/src/main/java/com/example/EmployeeManagementSystem/employee/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.EmployeeManagementSystem.employee.repository;
-
-import com.example.EmployeeManagementSystem.employee.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
